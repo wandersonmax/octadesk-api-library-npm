68297ace32ebedb404ff545010254e2c6594d71b
Rafael Henrique <rhsobr@gmail.com>
2018-03-12 20:34:09 -0300

    :fire: - Removing hardcoded API Token

--- test/api.tags.test.js
+++ test/api.tags.test.js
@@ -5,7 +5,7 @@ const Octadesk = require('..')
 
 const baseUrl = config.get("API_BASEURL")
 
-const octadesk = new Octadesk(baseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJkb21haW4iOiJ0ZXN0ZWJpZ291c3ByZCIsImlzc3VlZGF0ZSI6IjIwMTgtMDMtMTJUMjA6NTU6MzIuMTQ0WiIsImlzcyI6ImFwaS5vY3RhZGVzay5zZXJ2aWNlcyIsInJvbGUiOjIsImVtYWlsIjoicmFmYWVsLnNvYnJpbmhvQG9jdGFkZXNrLmNvbSIsIm5hbWUiOiJSYWZhZWwgSGVucmlxdWUiLCJ0eXBlIjoxLCJpZCI6IjU0ZDNiYzA0LWVmMjAtNDNlOS1iZTk2LWRhNzBkMzgyNzBiOSIsInJvbGVUeXBlIjoyLCJwZXJtaXNzaW9uVHlwZSI6MiwicGVybWlzc2lvblZpZXciOjEsImlhdCI6MTUyMDg4ODEzMn0.sK3Q-6Q-IFcxk27y_bdne_YD2xpE7xaT7Tt4QHv7xeo')
+const octadesk = new Octadesk(baseUrl, config.get("API_TOKEN"))
 
 describe('api.tags', () => {
   describe('get', () => {
